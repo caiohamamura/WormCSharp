80c1d55a8f08032e718d610d55fc8ed83d256e5f
Caio Hamamura <caiohamamura@gmail.com>
2021-07-09 18:12:30 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,274 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Threading.Tasks;
+// using System.Linq;
+
+// namespace worm
+// {
+//     public enum Direcao
+//     {
+//         cima,
+//         baixo,
+//         direita,
+//         esquerda
+//     }
+//     public struct Position
+//     {
+//         public Position(int x, int y)
+//         {
+//             this.x = x;
+//             this.y = y;
+//         }
+
+//         public override bool Equals(Object obj)
+//         {
+//             Position pos2 = (Position)obj;
+//             return (this.x == pos2.x && this.y == pos2.y);
+//         }
+
+//         public static bool operator ==(Position pos1, Position pos2)
+//         {
+//             return pos1.Equals(pos2);
+//         }
+
+//         public static bool operator !=(Position pos1, Position pos2)
+//         {
+//             return !pos1.Equals(pos2);
+//         }
+
+//         public override int GetHashCode()
+//         {
+//             return HashCode.Combine(this.x, this.y);
+//         }
+//         public int x { get; set; }
+//         public int y { get; set; }
+//     }
+//     public class Worm
+//     {
+//         const int initialSize = 3;
+//         public int tamanho = initialSize;
+//         public Position posicaoAtual = new Posicao(15, 10);
+//         public Direcao direcao = Direcao.direita;
+//         public Direcao direcaoAndada = Direcao.direita;
+//         public Queue<Position> corpo = new Queue<Position>();
+
+//         public Worm()
+//         {
+//             corpo.Enqueue(posicaoAtual);
+//             corpo.Enqueue(posicaoAtual);
+//             corpo.Enqueue(posicaoAtual);
+//         }
+
+//         public bool Anda()
+//         {
+//             switch (this.direcao)
+//             {
+//                 case Direcao.cima:
+//                     posicaoAtual.y -= 1;
+//                     break;
+//                 case Direcao.baixo:
+//                     posicaoAtual.y += 1;
+//                     break;
+//                 case Direcao.esquerda:
+//                     posicaoAtual.x -= 1;
+//                     break;
+//                 case Direcao.direita:
+//                     posicaoAtual.x += 1;
+//                     break;
+//             }
+//             if (corpo.Count == tamanho)
+//                 corpo.Dequeue();
+//             foreach (var ponto in corpo)
+//             {
+//                 if (posicaoAtual == ponto)
+//                 {
+//                     return false;
+//                 }
+//             }
+//             corpo.Enqueue(posicaoAtual);
+//             direcaoAndada = direcao;
+//             return true;
+//         }
+
+//         public void Come()
+//         {
+//             tamanho++;
+//         }
+
+//         public void TrocaDirecao(Direcao novaDirecao)
+//         {
+//             switch (novaDirecao)
+//             {
+//                 case Direcao.cima:
+//                     if (direcaoAndada == Direcao.baixo)
+//                         return;
+//                     break;
+//                 case Direcao.esquerda:
+//                     if (direcaoAndada == Direcao.direita)
+//                         return;
+//                     break;
+//                 case Direcao.direita:
+//                     if (direcaoAndada == Direcao.esquerda)
+//                         return;
+//                     break;
+//                 case Direcao.baixo:
+//                     if (direcaoAndada == Direcao.cima)
+//                         return;
+//                     break;
+//             }
+//             this.direcao = novaDirecao;
+
+//         }
+//     }
+
+//     public class Cenario
+//     {
+//         const double fatorVertical = 3.0 / 8.0;
+//         const int largura = 50;
+//         int altura = (int)(largura * fatorVertical);
+
+//         public Worm minhoca = new Minhoca();
+//         public Random rand = new Random();
+//         public Position comida;
+
+//         public Cenario()
+//         {
+//             DesenhaCenario();
+//             CriaComida();
+//         }
+
+//         public void CriaComida()
+//         {
+//             do
+//             {
+//                 comida.x = rand.Next(1, largura - 1);
+//                 comida.y = rand.Next(1, altura);
+//             } while (minhoca.corpo.Any(pos => pos == comida));
+
+//             Console.SetCursorPosition(comida.x, comida.y);
+//             Console.ForegroundColor = ConsoleColor.Green;
+//             Console.Write("O");
+//             Console.ForegroundColor = ConsoleColor.White;
+//         }
+
+//         public void DesenhaCenario()
+//         {
+//             Console.Clear();
+//             Console.ForegroundColor = ConsoleColor.White;
+//             Console.BackgroundColor = ConsoleColor.Black;
+//             Console.WriteLine(new String('-', largura));
+//             for (int ii = 0; ii < altura; ii++)
+//             {
+//                 Console.Write("|");
+//                 Console.Write(new String(' ', largura - 2));
+//                 Console.WriteLine("|");
+//             }
+//             Console.WriteLine(new String('-', largura));
+//             Console.Write("Pontuação: 0");
+//         }
+
+//         void DesenhaMinhoca()
+//         {
+//             Console.SetCursorPosition(minhoca.posicaoAtual.x, minhoca.posicaoAtual.y);
+//             Console.BackgroundColor = ConsoleColor.White;
+//             Console.Write("O");
+//             Console.BackgroundColor = ConsoleColor.Black;
+//         }
+
+//         public void AtualizaPontuacao()
+//         {
+//             Console.SetCursorPosition(11, altura + 2);
+//             Console.Write(minhoca.tamanho - 3);
+//         }
+
+
+//         public void LoopPrincipal()
+//         {
+//             Console.CursorVisible = false;
+//             DesenhaMinhoca();
+//             while (true)
+//             {
+//                 Position rabo = minhoca.corpo.Peek();
+//                 if (minhoca.Anda() == false ||
+//                     minhoca.posicaoAtual.x < 1 ||
+//                     minhoca.posicaoAtual.x > (largura - 2) ||
+//                     minhoca.posicaoAtual.y < 1 ||
+//                     minhoca.posicaoAtual.y > (altura))
+//                 {
+//                     goto Morreu;
+//                 }
+//                 DesenhaMinhoca();
+//                 if (minhoca.posicaoAtual == comida)
+//                 {
+//                     minhoca.Come();
+//                     AtualizaPontuacao();
+//                     CriaComida();
+//                 }
+//                 if (rabo != minhoca.posicaoAtual)
+//                     Console.SetCursorPosition(rabo.x, rabo.y);
+//                 Console.Write(" ");
+//                 Console.SetCursorPosition(largura - 1, altura + 2);
+//                 Console.Write(" ");
+//                 if (minhoca.direcaoAndada == Direcao.cima || minhoca.direcaoAndada == Direcao.baixo)
+//                     System.Threading.Thread.Sleep((int)(100 / fatorVertical));
+//                 else
+//                     System.Threading.Thread.Sleep(100);
+//             }
+//         Morreu:
+//             Console.SetCursorPosition(0, altura + 3);
+//             Console.WriteLine("Você morreu!", minhoca.tamanho);
+//             Console.ResetColor();
+//         }
+//     }
+//     class Program
+//     {
+//         static void LerTeclado(Worm minhoca)
+//         {
+//             while (true)
+//             {
+//                 var tecla = Console.ReadKey(true);
+
+//                 switch (tecla.Key)
+//                 {
+//                     case ConsoleKey.UpArrow:
+//                         minhoca.TrocaDirecao(Direcao.cima);
+//                         break;
+//                     case ConsoleKey.DownArrow:
+//                         minhoca.TrocaDirecao(Direcao.baixo);
+//                         break;
+//                     case ConsoleKey.LeftArrow:
+//                         minhoca.TrocaDirecao(Direcao.esquerda);
+//                         break;
+//                     case ConsoleKey.RightArrow:
+//                         minhoca.TrocaDirecao(Direcao.direita);
+//                         break;
+//                     default:
+//                         break;
+//                 }
+//             }
+//         }
+
+//         static void limpaCoresCtrlC(object sender, ConsoleCancelEventArgs args) {
+//             Console.ResetColor();
+//         }
+//         static void Main(string[] args)
+//         {
+//             Console.CancelKeyPress += new ConsoleCancelEventHandler(limpaCoresCtrlC);
+
+//             // Se der erro em algum lugar limpar as cores
+//             try
+//             {
+//                 Cenario cenario = new Cenario();
+//                 var task = Task.Run(() =>
+//                 {
+//                     LerTeclado(cenario.minhoca);
+//                 });
+//                 cenario.LoopPrincipal();
+//             } catch {
+//                 Console.ResetColor();
+//             }
+//         }
+
+
+//     }
+// }
---
+++ Program_en.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace worm
+{
+    public enum Direction
+    {
+        up,
+        down,
+        right,
+        left
+    }
+    public struct Position
+    {
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            Position pos2 = (Position)obj;
+            return (this.x == pos2.x && this.y == pos2.y);
+        }
+
+        public static bool operator ==(Position pos1, Position pos2)
+        {
+            return pos1.Equals(pos2);
+        }
+
+        public static bool operator !=(Position pos1, Position pos2)
+        {
+            return !pos1.Equals(pos2);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.x, this.y);
+        }
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+    public class Worm
+    {
+        const int initialSize = 3;
+        public int size = initialSize;
+        public Position currentPosition = new Position(15, 10);
+        public Direction direction = Direction.right;
+        public Direction wentDirection = Direction.right;
+        public Queue<Position> body = new Queue<Position>();
+
+        public Worm()
+        {
+            body.Enqueue(currentPosition);
+            body.Enqueue(currentPosition);
+            body.Enqueue(currentPosition);
+        }
+
+        public bool Move()
+        {
+            switch (this.direction)
+            {
+                case Direction.up:
+                    currentPosition.y -= 1;
+                    break;
+                case Direction.down:
+                    currentPosition.y += 1;
+                    break;
+                case Direction.left:
+                    currentPosition.x -= 1;
+                    break;
+                case Direction.right:
+                    currentPosition.x += 1;
+                    break;
+            }
+            if (body.Count == size)
+                body.Dequeue();
+            foreach (var point in body)
+            {
+                if (currentPosition == point)
+                {
+                    return false;
+                }
+            }
+            body.Enqueue(currentPosition);
+            wentDirection = direction;
+            return true;
+        }
+
+        public void Eat()
+        {
+            size++;
+        }
+
+        public void TrocaDirection(Direction newDirection)
+        {
+            switch (newDirection)
+            {
+                case Direction.up:
+                    if (wentDirection == Direction.down)
+                        return;
+                    break;
+                case Direction.left:
+                    if (wentDirection == Direction.right)
+                        return;
+                    break;
+                case Direction.right:
+                    if (wentDirection == Direction.left)
+                        return;
+                    break;
+                case Direction.down:
+                    if (wentDirection == Direction.up)
+                        return;
+                    break;
+            }
+            this.direction = newDirection;
+
+        }
+    }
+
+    public class GameScene
+    {
+        const double verticalFactor = 3.0 / 8.0;
+        const int width = 50;
+        int height = (int)(width * verticalFactor);
+
+        public Worm worm = new Worm();
+        public Random rand = new Random();
+        public Position food;
+
+        public GameScene()
+        {
+            Draw();
+            SpawnFood();
+        }
+
+        public void SpawnFood()
+        {
+            do
+            {
+                food.x = rand.Next(1, width - 1);
+                food.y = rand.Next(1, height);
+            } while (worm.body.Any(pos => pos == food));
+
+            Console.SetCursorPosition(food.x, food.y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("O");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void Draw()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(new String('-', width));
+            for (int ii = 0; ii < height; ii++)
+            {
+                Console.Write("|");
+                Console.Write(new String(' ', width - 2));
+                Console.WriteLine("|");
+            }
+            Console.WriteLine(new String('-', width));
+            Console.Write("Score: 0");
+        }
+
+        void DrawWorm()
+        {
+            Console.SetCursorPosition(worm.currentPosition.x, worm.currentPosition.y);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("O");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public void UpdateScore()
+        {
+            Console.SetCursorPosition(7, height + 2);
+            Console.Write(worm.size - 3);
+        }
+
+
+        public void MainLoop()
+        {
+            Console.CursorVisible = false;
+            DrawWorm();
+            while (true)
+            {
+                Position rabo = worm.body.Peek();
+                if (worm.Move() == false ||
+                    worm.currentPosition.x < 1 ||
+                    worm.currentPosition.x > (width - 2) ||
+                    worm.currentPosition.y < 1 ||
+                    worm.currentPosition.y > (height))
+                {
+                    break;
+                }
+                DrawWorm();
+                if (worm.currentPosition == food)
+                {
+                    worm.Eat();
+                    UpdateScore();
+                    SpawnFood();
+                }
+                if (rabo != worm.currentPosition)
+                    Console.SetCursorPosition(rabo.x, rabo.y);
+                Console.Write(" ");
+                Console.SetCursorPosition(width - 1, height + 2);
+                Console.Write(" ");
+                
+                System.Threading.Thread.Sleep(100);
+            }
+            Console.SetCursorPosition(0, height + 3);
+            Console.WriteLine("You're dead!");
+            Console.ResetColor();
+        }
+    }
+    class Program
+    {
+        static void ReadKeyboardAsync(Worm worm)
+        {
+            while (true)
+            {
+                var tecla = Console.ReadKey(true);
+
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        worm.TrocaDirection(Direction.up);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        worm.TrocaDirection(Direction.down);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        worm.TrocaDirection(Direction.left);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        worm.TrocaDirection(Direction.right);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void resetColorsCtrlC(object sender, ConsoleCancelEventArgs args)
+        {
+            Console.ResetColor();
+        }
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(resetColorsCtrlC);
+
+            // If there is any error reset the colors
+            try
+            {
+                GameScene gameScene = new GameScene();
+                var task = Task.Run(() =>
+                {
+                    ReadKeyboardAsync(gameScene.worm);
+                });
+                gameScene.MainLoop();
+            }
+            catch
+            {
+                Console.ResetColor();
+            }
+        }
+
+
+    }
+}
